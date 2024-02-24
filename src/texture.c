289b5a684b213c570f5f16be520cb41e169991d8
apollon64 <torbjoernhaugen@gmail.com>
2024-02-24 23:14:32 +0100

    Update texture.c
    Add extra prints for each bitdepth. Make it visible if we have 3 or 4 channels.

--- src/texture.c
+++ src/texture.c
@@ -13,29 +13,39 @@ texture_t* texture_list = NULL;
 
 texture_t load_png_texture_data(const char* filename) {
     printf("load:%s\n",filename);
-    upng_t* png_texture = upng_new_from_file(filename);
-    upng_error err = upng_get_error(png_texture);
+    upng_t* upng = upng_new_from_file(filename);
+    upng_error err = upng_get_error(upng);
     if (err == UPNG_ENOTFOUND)
     {
         fprintf(stderr, "upng_get_error: %d\n", err);
         assert(0);
     }
-    assert(upng_get_error(png_texture) == UPNG_EOK);
+    assert(upng_get_error(upng) == UPNG_EOK);
 
-    if (png_texture != NULL) {
+    if (upng != NULL) {
         printf("decode:%s\n", filename);
-        upng_decode(png_texture);
-        printf("get err:%s\n", filename);
-        upng_error err = upng_get_error(png_texture);
+        upng_decode(upng);
+        printf("check err:%s\n", filename);
+        upng_error err = upng_get_error(upng);
         if (err == UPNG_EOK) {
+            unsigned w = upng_get_width(upng);
+            unsigned h = upng_get_height(upng);
+            unsigned bpp = upng_get_bpp(upng);
+            unsigned bitdepth = upng_get_bitdepth(upng);
+            unsigned comp = upng_get_components(upng);
+            unsigned pixsiz = upng_get_pixelsize(upng);
+            upng_format	fmt = upng_get_format(upng);
+
             num_textures_loaded++;
             assert(num_textures_loaded < 256);
             texture_t texture;
             texture.handle = num_textures_loaded;
-            texture.upng_handle = (void*) png_texture;
-            texture.texels = (uint32_t*)upng_get_buffer(png_texture);
-            texture.width = upng_get_width(png_texture);
-            texture.height = upng_get_height(png_texture);
+            texture.upng_handle = (void*) upng;
+            texture.texels = (uint32_t*)upng_get_buffer(upng);
+            texture.width = w;
+            texture.height = h;
+            printf("Loaded %dx%d bpp[%d] bitdepth[%d] components[%d] pixsiz[%d] fmt[%d] %s\n",
+                   w, h, bpp, bitdepth, comp, pixsiz, fmt, filename);
             array_push(texture_list, texture);
             return texture;
         } else {
